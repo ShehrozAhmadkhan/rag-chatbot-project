a2e6c281f480a5a341d34ea0e7b50caabaaa1a68
Shehroz Ahmad <shehrozahmadrskh1234@gmail.com>
2026-06-24 02:45:16 +0500

    "Day 3 extra: interactive query with user input"

--- pinecone_test.py
+++ pinecone_test.py
@@ -28,4 +28,18 @@ for vector in range(len(chunks)):
 
 index.upsert(vectors=vectors)
 
-print(f"The lenght of vector is {len(vectors)}")
+
+
+#user_question = "What is the dog doing?"
+user_question = input("Ask a question: ")
+
+embed = client.embeddings.create(model="text-embedding-3-small",input=user_question,dimensions=1024)
+lofn = embed.data[0].embedding
+
+result = index.query(vector=lofn,top_k=1,include_metadata=True)
+
+print(result.matches[0].metadata["text"])
+
+#for chunk in result.matches:
+#    print(f"The sentence is {chunk.metadata["text"]} and the cosine similarity is {chunk.score}")
+
